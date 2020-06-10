42399b8fee40476bcef0913ac5104c1144b0bf4b
rjzupkoii <rjzii@yahoo.com>
2020-06-09 20:59:17 -0400

    Deleting old code

--- WebUI/Home/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.runningMasim, name='runningMasimHome'),
-    path('Masim', views.runningMasim, name='runningMasim'),
-    path('BurkinaFaso', views.runningBurkinaFaso, name = 'runningBurkinaFaso')
-]
--- WebUI/Latest/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('Masim', views.latestMasim, name='latestMasim'),
-    path('BurkinaFaso', views.latestBurkinaFaso, name = 'latestBurkinaFaso')
-]
