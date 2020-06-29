a44bf1f534c5c6ef613297a4425f95cbb615e9ad
rjzupkoii <rjzii@yahoo.com>
2020-06-29 14:00:50 -0400

    Create view

--- WebUI/lims/AppDatabase.py
+++ WebUI/lims/AppDatabase.py
@@ -10,6 +10,11 @@ class AppDatabase:
     ADMIN     = "host=masimdb.vmhost.psu.edu dbname=masim user={} password={}"
     CLONE     = "CREATE DATABASE {} WITH TEMPLATE template_masim"
     REFERENCE = "host=masimdb.vmhost.psu.edu dbname={} user=sim password=sim"
+    VIEW      = """CREATE OR REPLACE VIEW public.v_replicates AS SELECT c.filename, r.id, 
+                   r.configurationid, r.seed, r.starttime, r.endtime, r.movement, 
+                   r.endtime - r.starttime AS runningtime FROM sim.replicate r JOIN 
+                   sim.configuration c ON c.id = r.configurationid;
+                   ALTER TABLE public.v_replicates OWNER TO sim;"""
 
 
     def cloneDatabase(self, username, password, database):
@@ -24,9 +29,15 @@ class AppDatabase:
         connection.autocommit = True
         cursor = connection.cursor()
         cursor.execute(query)
+
+        # Add the view to the database
+        cursor.execute(VIEW)
+
+        # Clean-up and return
         cursor.close()
         connection.close()
 
 
     def createConnectionString(self, database):
         return self.REFERENCE.format(database.lower())
+
